b5d01a8efbbedc9c9ce12ff36e7f58b3395fcc92
thegang2 <137846546+thegang2@users.noreply.github.com>
2023-09-09 14:34:50 +0300

    Add files via upload

---
+++ SQL/extras/Source.py
@@ -0,0 +1,11 @@
+import re
+import time
+from datetime import datetime
+from Arab import StartTime, @iqLeaderThon
+from Arab.Config import Config
+from Arab.plugins import mention
+help1 = ("**🝳 ⦙ كيفيه التنصيب :**")
+help2 = ("**🝳 ⦙ قـائمـه الاوامـر :**\n**🝳 ⦙ قنـاه السـورس :** @iqLeaderThon\n**🝳 ⦙ شـرح اوامـر السـورس : @Leaderriq**\n**🝳 ⦙ شـرح فـارات السـورس : @TEAMTELETHON** \n - اوامر الاونلاين تشتغل فقط في المجموعات ")
+TG_BOT = Config.TG_BOT_USERNAME
+TM = time.strftime("%I:%M")
+Sour = f"**‎⿻┊My 𖠄 {mention} ٫ **\n‌‎**⿻┊BoT 𖠄 {TG_BOT} ٫**\n**‌‎⿻┊TimE 𖠄 {TM} ٫**\n**‌‎⿻┊‌‎VeRsIoN 𖠄 (7.7) ,** \n**⿻┊‌‎LeaderThon𖠄** @iqLeaderThon"
